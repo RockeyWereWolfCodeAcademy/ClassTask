19c61d45c6a518d0b16520fff0059f8fc18fbe77
RockeyWereWolf <mi71k9d7p@code.edu.az>
2023-10-19 20:05:58 +0400

    uploaded project

---
+++ ClassTask/ClassTask/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ClassTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User firstUser = new User("rockeywerewolf", "notmyrealpasswordlol");
+            firstUser.Login("rockeywerewolf", "myrealpassword123");
+            firstUser.Login("rockeywerewolf", "notmyrealpasswordlol");
+            firstUser.Login("rockeywerewolf", "notmyrealpasswordlol");
+            firstUser.Logout();
+            firstUser.Logout();
+            User secondUser = new User("ulviabdullazada", "123495678");
+            secondUser.Login("ulviabdullazada", "123495678");
+            secondUser.Login("ulviabdullazada", "123495678");
+            secondUser.Logout();
+            secondUser.Logout();
+        }
+    }
+}
---
+++ ClassTask/ClassTask/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask
+{
+    public class User
+    {
+        public string Username;
+        public string Password;
+        public bool IsLoggedIn;
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+            IsLoggedIn = false;
+        }
+
+        public void Login(string username, string password)
+        {
+            if (IsLoggedIn)
+            {
+                Console.WriteLine($"User {Username} already logged in.");
+            }
+            else if (Username == username && Password == password)
+            {
+                IsLoggedIn = true;
+                Console.WriteLine($"User {Username} is logged into the account.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid username or password.");
+            }
+        }
+
+        public void Logout()
+        {
+            if (IsLoggedIn)
+            {
+                IsLoggedIn = false;
+                Console.WriteLine($"User {Username} logged out.");
+            }
+            else
+            {
+                Console.WriteLine($"User {Username} is not logged in.");
+            }
+        }
+    }
+}
